cdf779b436dd76d2e6ae47d4403e073f14899c08
valpedro59 <valpedro59@gmail.com>
2024-01-17 10:25:49 +0100

    first commit

--- index.js
+++ index.js
@@ -20,8 +20,11 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/whoami', function (req, res) {
+  let ipadress = req.ip;
+let language = req.acceptsLanguages();
+let software=req.get('User-Agent');
+  res.json({ ipaddress: ipadress, language: language[0], software: software});
 });
 
 // listen for requests :)
